1c9057a0ce7a7ef4c9f81bf095b5db72c388ed65
Misi <tatai.misi@gmail.com>
2022-04-28 09:26:39 +0200

    changed depot to vehicle in mainapp

---
+++ darpVehicle.m
@@ -0,0 +1,17 @@
+classdef darpVehicle
+    
+   properties
+       vehicleId;
+       depotNodeId;
+       capacity;
+   end
+   
+   methods
+       
+   end
+   
+   methods (Access = private)
+   
+   end
+    
+end
